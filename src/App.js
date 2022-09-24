8db65446da96bc4030612ce55a66b0a937a6369d
PolyakovaMargarita <margaritka251289@gmail.com>
2022-09-25 01:28:27 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { AppBar, Container, IconButton, Toolbar, Typography, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(1)
+  },
+  title: {
+    flexGrow: 1
+  },
+  buttonMargin: {    
+    margin: '25px',   
+  },
+}))
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar position='fixed'>
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label='menu' className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>Material Site Practice</Typography>
+          <Box className={classes.buttonMargin}>
+            <Button color='inherit' variant='outlined'>Sign In</Button>
+          </Box>
+          <Button color='secondary' variant='contained'>Sign Up</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
