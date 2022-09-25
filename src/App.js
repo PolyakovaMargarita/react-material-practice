cb6e9e3ceeadd9cd1d0af61918a638c669d4a214
PolyakovaMargarita <margaritka251289@gmail.com>
2022-09-25 23:03:02 +0300

    add blogs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import { AppBar, Container, IconButton, Toolbar, Typography, Box, Grid, Paper } from '@material-ui/core';
+import { AppBar, Container, IconButton, Toolbar, Typography, Box, Grid, Paper, Card, CardMedia, CardContent, CardActions } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import LayerIcon from '@material-ui/icons/Layers';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -37,10 +39,21 @@ const useStyles = makeStyles((theme) => ({
   },
   mainFeaturesPostContent: {
     position: "relative",
-    padding: theme.spacing(11)
+    padding: theme.spacing(6),
+    marginTop: theme.spacing(9)
+  },
+  cardMedia: {
+    paddingTop: "56.25%"
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  cardGrid: {
+    marginTop: theme.spacing(4)
   }
 }))
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function App() {
   const classes = useStyles();
@@ -87,12 +100,66 @@ function App() {
                   >
                     MUI offers a comprehensive suite of UI tools to help you ship new features faster. Start with Material UI, our fully-loaded component library, or bring your ...
                   </Typography>
-                  <Button variant='conteined' color='secondary'>Learn more</Button>
+                  <Button variant='contained' color='secondary'>Learn more</Button>
                 </div>
               </Grid>
             </Grid>
           </Container>
         </Paper>
+        <div className={classes.mainContent}>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Blog Material
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+            MUI offers a comprehensive suite of UI tools to help you ship new features faster. Start with Material UI, our fully-loaded component library, or bring your ...
+            </Typography>
+            <div className={classes.mainButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant='contained' color='primary'>Start now</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>Learn more</Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                      Blog post
+                    </Typography>
+                    <Typography variant='h5' gutterBottom>
+                      Blog post. Different some posts
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small' color="primary">
+                      View
+                    </Button>
+                    <Button size='small' color="primary">
+                      Edit
+                    </Button>
+
+                    <LayerIcon />
+                    <PlayCircleFilledIcon />
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
 
